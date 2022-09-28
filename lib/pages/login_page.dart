788d0d0a7e191255642d576b5ac54f3a6a0871e6
ankitabadhe <ankitabadhe340@gmail.com>
2022-09-28 21:47:05 +0530

    DAY-5

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,28 +9,34 @@ class LoginPage extends StatelessWidget {
         color: Colors.white,
         child: Column(children: [
           Image.asset("assets/images/login_img.png", fit: BoxFit.cover),
-          const SizedBox(
-            height: 10.0,
-          ),
-          const Text("WELCOME",
+          const SizedBox(height: 20.0),
+          const Text("WELL-COME",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 10.0),
           Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                      decoration: const InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  )),
-                  TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                      ))
-                ],
-              ))
+              child: Column(children: [
+                TextFormField(
+                    decoration: const InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                )),
+                TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    )),
+                const SizedBox(height: 40.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // ignore: avoid_print
+                    print("Hi Ankita");
+                  },
+                  style: TextButton.styleFrom(),
+                  child: const Text("LOG-IN"),
+                )
+              ]))
         ]));
   }
 }
